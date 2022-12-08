cc236dfe3d5beb2b3e017c744709ea43aa2cba6c
felix <fkuhbier@stud.hs-heilbronn.de>
2022-12-08 11:51:13 +0100

    deleted file

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module de.hhn.ai.pmt.buga23.buga23ai {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens de.hhn.ai.pmt.buga23.buga23ai to javafx.fxml;
-    exports de.hhn.ai.pmt.buga23.buga23ai;
-}
