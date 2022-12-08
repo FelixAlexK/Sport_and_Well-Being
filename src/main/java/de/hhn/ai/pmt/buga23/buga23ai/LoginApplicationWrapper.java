ec7945707e1980d52a9edaa42e67708729b8cf45
felix <fkuhbier@stud.hs-heilbronn.de>
2022-12-08 11:50:32 +0100

    added wrapper classes

---
+++ src/main/java/de/hhn/ai/pmt/buga23/buga23ai/LoginApplicationWrapper.java
@@ -0,0 +1,8 @@
+package de.hhn.ai.pmt.buga23.buga23ai;
+
+public class LoginApplicationWrapper {
+    public static void main(String[] args) {
+        LoginApplication.main(args);
+    }
+
+}
---
+++ src/main/java/de/hhn/ai/pmt/buga23/buga23ai/RegisterApplicationWrapper.java
@@ -0,0 +1,9 @@
+package de.hhn.ai.pmt.buga23.buga23ai;
+
+public class RegisterApplicationWrapper {
+
+    public static void main(String[] args) {
+        RegisterApplication.main(args);
+    }
+
+}
