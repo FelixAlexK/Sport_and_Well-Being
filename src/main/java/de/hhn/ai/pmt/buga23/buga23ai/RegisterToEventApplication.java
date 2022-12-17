4f9973bb779881291af7f587c2d759e90e68224d
PascalGaugler <pgaugler@stud.hs-heilbronn.de>
2022-12-17 17:50:56 +0100

    Implemented data from Database

--- src/main/java/de/hhn/ai/pmt/buga23/buga23ai/RegisterToEventApplication.java
+++ src/main/java/de/hhn/ai/pmt/buga23/buga23ai/RegisterToEventApplication.java
@@ -9,7 +9,6 @@ import java.io.IOException;
 
 public class RegisterToEventApplication extends Application {
 
-
     public static void main(String[] args) {
         launch(args);
     }
--- src/main/java/de/hhn/ai/pmt/buga23/buga23ai/RegisterToEventController.java
+++ src/main/java/de/hhn/ai/pmt/buga23/buga23ai/RegisterToEventController.java
@@ -1,13 +1,13 @@
 package de.hhn.ai.pmt.buga23.buga23ai;
 
-import javafx.fxml.FXML;
-
+import de.hhn.ai.sportwellbeing.*;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
-
+import java.sql.Time;
+import java.util.Date;
 import java.util.Optional;
 
 public class RegisterToEventController {
@@ -39,9 +39,48 @@ public class RegisterToEventController {
     @FXML
     private Label labelStageNumber;
 
+    @FXML
+    private Label labelMaxParticipants;
 
+    @FXML
+    private Label labelNumberRegistarions;
+
+    @FXML
     public void initialize() {
 
+        try {
+            Event event = EventDAO.getEventByORMID(1);
+            Place place = PlaceDAO.getPlaceByORMID(1);
+
+            String eventName = event.getEventName();
+            labelEventName.setText(eventName);
+
+            PersonSetCollection eventAnimator = event.animator;
+            Person[] animator = eventAnimator.toArray();
+            labelAnimatorName.setText(String.valueOf(animator[0].getName()));
+
+            labelEventPrice.setText(event.getPrices()[0] / 100 + "€");
+
+            Date eventDate = event.getDate();
+            labelEventDate.setText(String.valueOf(eventDate));
+
+            Time eventTime = event.getTime();
+            labelEventTime.setText(String.valueOf(eventTime));
+
+            int eventStage = place.getNumber();
+            labelStageNumber.setText(String.valueOf(eventStage));
+
+            EventProvider eventOrganizer = event.getEventProvider();
+            labelEventProvider.setText(String.valueOf(eventOrganizer.getOrganization()));
+
+            int numberRegistrations = event.getNumberOfRegistrations();
+            labelNumberRegistarions.setText(String.valueOf(numberRegistrations));
+            int numberMaxRegistrations = event.getMaximumParticipants();
+            labelMaxParticipants.setText(String.valueOf(numberMaxRegistrations));
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 
     @FXML
@@ -57,11 +96,23 @@ public class RegisterToEventController {
 
         Optional<ButtonType> result = alert.showAndWait();
         if (result.get() == ButtonType.OK) {
-            //Open Menu and increase number of registrations
+            try {
+            Event event = EventDAO.getEventByORMID(1);
+            int numberRegistrations = event.getNumberOfRegistrations();
+            int newRegistrations = numberRegistrations+1;
+            event.setNumberOfRegistrations(newRegistrations);
+
+            int maxParticipants = event.getMaximumParticipants();
+            int newMaxParticipants = maxParticipants-1;
+            event.setMaximumParticipants(newMaxParticipants);
+            EventDAO.save(event);
+            EventDAO.refresh(event);
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
         }
         else {
             //Open Menu
         }
     }
-
 }
